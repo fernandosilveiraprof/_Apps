d980f4aca11c3296956af797deca140c96712de5
fernandosilveiraprof <fernandosilveiraprof@gmail.com>
2021-05-25 14:15:58 -0300

    UI footer checkAcces

---
+++ APIs/_front/CadastroOS.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title> Cadastro de OS</title>
+	<link rel="stylesheet" href="">
+
+	<style type="text/css" media="screen">
+
+		footer{
+			text-align: center;
+
+		}
+	</style>
+</head>
+<body>
+	<form action="CadastroOS_submit" method="post" accept-charset="utf-8">
+		<article>
+			<label for=""> Nº </label>
+			<div id="N_OS">
+				
+			</div>
+			<label for="">
+				
+			</label>
+			
+
+
+
+
+
+
+
+		</article>
+	</form>
+	<?php 
+	require 'CheckAccess.php'; 
+
+	?>
+</body>
+</html>
---
+++ APIs/_front/CheckAccess.php
@@ -0,0 +1,17 @@
+
+
+<footer>
+	<form action="" method="post" accept-charset="utf-8">
+		
+		
+		<label for="usuário"> Login </label>
+		<select name="usuario">
+			<option value="FERNANDOS">FERNANDO SILVEIRA</option>
+			<option value="ROBSON">ROBSON</option>
+
+			
+		</select>
+		<label for="senha"> Senha </label>
+		<input type="password" name="senha">
+	</form>
+</footer>
