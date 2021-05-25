4e60edf0d1621b956b1daad2e78606fb09eedd39
fernandosilveiraprof <fernandosilveiraprof@gmail.com>
2021-05-25 14:26:29 -0300

    add archives

---
+++ APIs/_front/menu.php
@@ -0,0 +1,8 @@
+<ul>
+	<li><a href="http://localhost/_Apps/APIs/_front/"> Início </a></li>
+	<li><a href="http://localhost/_Apps/APIs/_front/VisualizarOS.php">Visualizar OS</a></li>
+	<li><a href=""></a></li>
+	<li><a href=""></a></li>
+	<li><a href=""></a></li>
+	<li><a href=""></a></li>
+</ul>
