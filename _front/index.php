73eec7963913da6fe9c51dd135bc2e42e8308ffa
fernandosilveiraprof <fernandosilveiraprof@gmail.com>
2021-05-31 15:25:50 -0300

    atualizando

--- _front/index.php
+++ _front/index.php
@@ -5,37 +5,93 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>	Sistema O.S </title>
 	<link rel="stylesheet" href="">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<style>		
+		*{
+
+			margin: 0px ;
+			padding: 0px;
+			border:1px solid red;
+			font-family: Arial;
+			font-size: 18pt;
+
+		}
+
+		body{
+
+			width: 100vw;
+			height: 100vh;
+
+		}
+		form{
+
+			display: flex; 
+			flex-direction: column;
+
+			justify-content: center; 
+			align-items: center ;
+
+		}
+
+		.linhaForm > input{
+			line-height: 30px;
+		}
+
+		@media screen and (max-width: 360px){
+
+			.linhaForm {
+				line-height: 12px;
 
 
-	<style>			
-		
+			}
+		}
+
 	</style>	
 
 
 </head>
-<body>
-	<form action="verificarlogin.php">
+<body style=" ">
+	
+	<form action="verificarlogin.php" style="width: 100%; height: 100%; ">
+
 		<div class="linhaForm" >	
-				<figure>	
-						<img src="logo.png" alt="Logo da Empresa">
-				</figure>	
-		</div>
-		<div class="linhaForm" >	
-		
-			<label for="login">Login</label>
-			<input type="text" id="login" name="login">
-		
+
+			<figure>	
+
+				<img src="_imgs/logo.jpg" alt="Logo da Empresa" style="width: 100px ; height: 100px; ">
+
+			</figure>	
+
 		</div>
 
-			<label for="senha"  >
-				Senha: 
-			</label>
+		<div class="linhaForm" >	
+
+			<label for="login"> Login: </label>
+			<input type="text" id="login" name="login">
+
+		</div>
+
+		<div class="linhaForm">	
+
+			<label for="senha">	Senha: 	</label>
 			<input type="password" name="senha" >
 
-		<div class="linhaForm" >	
-				<a href="">Esqueceu sua senha? </a> 
+		</div>
+
+		<div class="linhaForm" style="text-align: right;">	
+			
+			<a href=""> Esqueceu sua senha? </a> <br>
 
 		</div>
+
+		<div class="linhaForm" style="text-align: right;">	
+
+			<a href=""> Cadastre-se Como Novo Usuário! </a> 
+
+		</div>
+
+		
 	</form>
 </body>
 </html>
