7be782ebf78e48b0c5542bd76cc5633c97049989
fernandosilveiraprof <fernandosilveiraprof@gmail.com>
2021-06-02 16:17:19 -0300

    Modificando estrutura do menu e adicionando links sem href

--- _front/menu.php
+++ _front/menu.php
@@ -1,10 +1,32 @@
 <nav>
-	<ul>
-		<li><a href="http://localhost/_Apps/APIs/_front/"> Início </a></li>
-		<li><a href="http://localhost/_Apps/APIs/_front/VisualizarOS.php">Visualizar OS</a></li>
-		<li><a href="http://localhost/_Apps/APIs/_front/">Cadastrar Cliente</a></li>
-		<li><a href="http://localhost/_Apps/APIs/_front/"> Cadastrar Funcionário</a></li>
-		<li><a href="http://localhost/_Apps/APIs/_front/"></a></li>
-		<li><a href="http://localhost/_Apps/APIs/_front/"></a></li>
-	</ul>
+	<label for="equiv"> &equiv; </label>
+	<input type="checkbox" id="equiv">
+
+	<div class="displayMenu">
+
+		<a href="painel.php"> Início </a>
+
+
+		<label for="sub-1"> Cadastros </label>
+		<div class="sub" id="sub-1">
+			<a href=""> Cadastrar Client </a>
+			<a href=""> Cadastrar Funcionário </a>
+			<a href=""> Cadastrar Serviço </a>
+			<a href=""> Cadastrar Produtos </a>
+			<a href=""> Cadastrar Ordem de Serviços </a>
+		</div>
+
+		<label for="">
+			Visualizar 
+		</label>
+		<div class="sub" id="sub-2">
+			<a href=""> Visualizar Client </a>
+			<a href=""> Visualizar Funcionários </a>
+			<a href=""> Visualizar Serviço </a>
+			<a href=""> Visualizar Produtos </a>
+			<a href=""> Visualizar Ordem de Serviço </a>
+		</div>
+		
+	</div>
+	<label for=""></label>
 </nav>
